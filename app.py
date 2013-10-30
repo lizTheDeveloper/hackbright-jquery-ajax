e5da09ddda1dc2b776620e66ed22cc68ab0e6958
Liz Howard <ann.howard@me.com>
2013-10-30 15:34:30 -0700

    changed links to point to teaching materials for now, make my own slides laters

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 user_tasks = [{"title":"a task", "description":"A test task", "id":0, "complete": False}]
 
 @app.route("/")
-def example():
+def home():
     return render_template("home.html")
 
 @app.route("/1")
@@ -15,6 +15,14 @@ def example():
 @app.route("/2")
 def example2():
     return render_template("example2.html")
+
+@app.route("/slides/jquery")
+def jquery():
+    return render_template("jquery_slides.html")
+
+@app.route("/slides/ajax")
+def ajax():
+    return render_template("ajax_slides.html")
 
 @app.route("/tasks/add")
 def add_task():
