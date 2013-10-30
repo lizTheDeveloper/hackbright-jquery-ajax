0516b571a0fa73be0a13314c02e117580235c7b7
Liz Howard <ann.howard@me.com>
2013-10-30 15:01:01 -0700

    more structure

--- app.py
+++ app.py
@@ -1,35 +1,40 @@
-from flask import Flask, g
+from flask import Flask, render_template
 import json
 app = Flask(__name__)
 
-g.tasks = []
+user_tasks = [{"title":"a task", "description":"A test task", "id":0, "complete": False}]
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def example():
+    return render_template("example.html")
+
+@app.route("/2")
+def example2():
+    return render_template("example2.html")
 
 @app.route("/tasks/add")
 def add_task():
     task = {"title" : request.form.get("title", "Untitled Task"),
     "description" : request.form.get("description", ""),
-    id: len(g.tasks)}
-    g.tasks.append(task)
+    "id": len(user_tasks),
+    "complete": False}
+    user_tasks.append(task)
     return json.dumps(task)
 
 
 @app.route("/tasks/delete/<task_id>")
 def del_task(task_id):
-    for i in range(0,len(g.tasks)):
-        if g.tasks[i]["id"] == task_id:
-            del g.tasks[i]
+    for i in range(0,len(user_tasks)):
+        if user_tasks[i]["id"] == task_id:
+            del user_tasks[i]
             return "success"
     return "error - task not found"
 
 
 @app.route("/tasks")
 def tasks():
-    return json.dumps(g.tasks)
+    return json.dumps(user_tasks)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
